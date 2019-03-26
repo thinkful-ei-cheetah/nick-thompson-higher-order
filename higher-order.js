d1e4b9f9614e945321212bb1355fbf6265cc04c0
Nick Lee <leexnick@gmail.com>
2019-03-26 12:07:37 -0700

    add filter()

--- higher-order.js
+++ higher-order.js
@@ -1,19 +1,51 @@
+'use strict';
 
-
-funciton Repeat(fn, n){
-    for(i=0;i<n;i++){
-        fn();
-    }
-return ;
+function repeat(fn, n){
+  for(let i=0;i<n;i++){
+    fn();
+  }
 }
 
 function hello(){
-    console.log('Hello world');
+  console.log('Hello world');
 }
 
 function goodbye(){
-    console.log('Goodbye world');
+  console.log('Goodbye world');
 }
 
-Repeat(hello, 5);
-Repeat(goodbye, 5);
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+
+
+function filter(arr, fn) {
+  const newArray = [];
+
+  arr.forEach(element => {
+    fn(element) === true ? newArray.push(element) : undefined ;
+  });
+
+  return newArray; 
+  // // arr = ['Rich', 'Joe', 'Bhaumik', 'Ray']
+
+  // return arr.map(fn); //[true, false, false, true]
+
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+//BONUS
+console.log(filter(myNames, (name) => name[0] === 'R'));
